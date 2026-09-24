94054ec7fc8f53e4acd4ebffcd9eb9b6af751ffc
leonid vedernikov <leonid.vedernikov2206@mail.ru>
2026-09-24 14:18:55 +0400

    Revert "Implement auto service REST API and React client"
    This reverts commit 068c8c49f5786885a7a5ece2b1c7342ccda7bd98.

--- backend/src/main/java/ru/webip/autoservice/exception/AppointmentNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.webip.autoservice.exception;
-
-public class AppointmentNotFoundException extends RuntimeException {
-
-    public AppointmentNotFoundException(Long id) {
-        super("Запись с идентификатором " + id + " не найдена");
-    }
-}
--- backend/src/main/java/ru/webip/autoservice/model/AppointmentStatus.java
+++
@@ -1,9 +0,0 @@
-package ru.webip.autoservice.model;
-
-public enum AppointmentStatus {
-    NEW,
-    CONFIRMED,
-    IN_PROGRESS,
-    COMPLETED,
-    CANCELLED
-}
